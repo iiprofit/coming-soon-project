e6578b092b72ce79f7f51653350216e8e5e59452
Kirtirajsinh Solanki <kirtiraj2310@gmail.com>
2022-06-21 12:37:55 +0530

    mail updated

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends React.Component {
         <div className="header">
           <div className="logo">
             {/* <a href="."><img src={cmpLogo} alt="Logo" width="175px" height="150px" /></a> */}
-            <h3>Shadev Infotech</h3>
+            <h3>Nirvaahak Exim</h3>
           </div>
           <div className="social">
-            <a href="https://www.facebook.com/ShadevInfotech" title="Facebook" target="_blank" >
+            <a href="#" title="Facebook" target="_blank" >
               <IconFacebook className="icon" />
             </a>
             <a href="#" title="Twitter" target="_blank" >
@@ -31,12 +31,12 @@ class App extends React.Component {
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:email@example.com">
+          <a href="mailto:info@nirvaahakexim.com">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="#" target="_blank" >Shadev Infotech</a> </span>
+          {/* <span>made by <a className="underlined" href="#" target="_blank" >Ni</a> </span> */}
         </div>
       </div>
     );
